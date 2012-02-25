fa1ae794600fd309f90dcdb262587e8b1d59ddfd
Élie Bouttier <cube.elie@gmail.com>
2012-02-25 18:33:35 +0100

    La mission petit fait le tour complet du plateau

--- ia/Petit.cxx
+++ ia/Petit.cxx
@@ -11,19 +11,39 @@ class Petit: public Mission
 
 	void run() {
 		printf("Petit::run\n");
-		state = 0;
-		can->fwd(200);
+		state = -1;
 		wait();
+		mh->load("init");
+		//wait();
 	}
 
 	void microswitch(int id, bool status) {
-		printf("Petit::microswitch: id = %d\n", id);
-		//printf("statengo::microswitch: id = %d, state = %d\n", id, state);
+		printf("Petit::microswitch: id = %d, status = %d\n", id, status);
 		switch (state) {
+			case -1:
+				if (id == 0) {
+					state = 0;
+					usleep(100000);
+					can->fwd(298);
+				}
+				break;
 			case 7:
 				if (id == 1) {
+					usleep(300000);
 					can->stop();
-					signal();
+					usleep(100000);
+					can->fwd(-200);
+					state = 8;
+					//signal();
+				}
+				break;
+			case 52:
+				if (id == 1) {
+					usleep(300000);
+					can->stop();
+					usleep(100000);
+					can->fwd(-200);
+					state = 53;
 				}
 				break;
 		}
@@ -39,7 +59,7 @@ class Petit: public Mission
 				break;
 			case 1:
 				state = 2;
-				can->fwd(800);
+				can->fwd(600);
 				break;
 			case 2:
 				state = 3;
@@ -60,6 +80,56 @@ class Petit: public Mission
 			case 6:
 				state = 7;
 				can->fwd(80,80);
+				usleep(2000000);
+				can->fwd(30,30);
+				break;
+			case 8:
+				state = 9;
+				can->rotate(-90);
+				break;
+			case 9:
+				state = 50;
+				can->fwd(1234);
+				break;
+			case 50:
+				state = 51;
+				can->rotate(90);
+				break;
+			case 51:
+				state = 52;
+				can->fwd(20, 20);
+				break;
+			case 53:
+				state = 54;
+				can->rotate(-90);
+				break;
+			case 54:
+				state = 10;
+				can->fwd(575);
+				break;
+			case 10:
+				state = 11;
+				can->rotate(-90);
+				break;
+			case 11:
+				state = 12;
+				can->fwd(1500);
+				break;
+			case 12:
+				state = 13;
+				can->rotate(-87);
+				break;
+			case 13:
+				state = 14;
+				can->fwd(1700);
+				break;
+			case 14:
+				state = 15;
+				can->rotate(180);
+				break;
+			case 15:
+				state = 16;
+				signal();
 				break;
 		}
 	}
